aaf74fefeba2b60a35503b5be3afd22ecf1a24d9
Bearuser <bearuser@codelion.net>
2015-01-14 02:32:09 +0000

    실제 이메일 기능 탑재하기

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -4,6 +4,18 @@ class EmailController < ApplicationController
 
 	def email_send
 		@email = params[:email]
+		@title = params[:title]
 		@content = params[:content]
+
+		gmail = Gmail.connect("kougayun@gmail.com","bjs227544!")
+		gmail.deliver do
+			to email
+			subject title
+			text_part do
+				body content
+			end
+		end
+
+		gmail.logout
 	end
 end
